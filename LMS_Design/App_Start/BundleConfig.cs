b8622ef5f70f39dab99cecd210ec8f2cf1fe6282
hrosado <hommy.rosado@gmail.com>
2020-07-30 17:46:18 -0400

    Setup initial files. Need to modify the collapse / responsive menu.

--- LMS_Design/App_Start/BundleConfig.cs
+++ LMS_Design/App_Start/BundleConfig.cs
@@ -20,10 +20,12 @@ namespace LMS_Design
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/LMSTemplateJs/theme.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/LMSTemplateCss/theme.min.css",
                       "~/Content/site.css"));
         }
     }
